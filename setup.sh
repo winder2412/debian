a56a22a2defa9d3a08303b07b762e7add5f646f2
winder1234 <winderteppei.dm@gmail.com>
2023-05-08 08:40:15 +0330

    Update May 8th

--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
-apt update && apt upgrade
+#!/bin/bash
+ISYAY=/sbin/yay
+if [ -z "$(command)" ]; then
+    grep "deb https://deb.debian.org/debian/ unstable main" /etc/apt/sources.list
+    sudo echo "deb https://deb.debian.org/debian/ unstable main
+    deb-src https://deb.debian.org/debian/ unstable main" >> /etc/apt/sources.list
+    apt update && apt upgrade
+fi
+else 
+    echo -e "sid branch is already used,moving on"   
+    apt update && apt upgrade
+fi
+
+
